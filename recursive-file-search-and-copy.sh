94d3d1d81b2660d6718d6349c68c4ddd003afe63
Diogo Fiaminghi <diogofiaminghi@gmail.com>
2023-05-11 09:29:39 -0300

    Create recursive-file-search-and-copy.sh

---
+++ recursive-file-search-and-copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Asks the user to enter parts of the filenames to search for, separated by spaces
+read -p "Enter part(s) of the filename(s) to search for (separated by spaces): " -a search_names
+
+# Asks the user to enter the name of the folder to be created inside /tmp directory
+read -p "Enter the name of the folder to be created inside /tmp directory: " folder_name
+
+# Defines the full path of the destination directory
+dest="/tmp/$folder_name"
+
+# Creates the new destination folder if it doesn't exist
+mkdir -p "$dest"
+
+# Finds all files containing any of the specified name parts in the current directory and its subdirectories,
+# and copies each found file to the destination directory
+for name_part in "${search_names[@]}"
+do
+    find . -type f -name "*$name_part*" -exec cp {} "$dest" \;
+done
